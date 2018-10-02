de922a8aec4cf1cab64bd7b2bea1f81e9b3ebe80
craigsdennis <craig@teamtreehouse.com>
2018-10-02 15:36:13 -0700

    Adds some more metadata to the generation script

--- th-mark-clip.sh
+++ th-mark-clip.sh
@@ -1,2 +1,9 @@
-{ echo '<span class="markdown--jupyter-notebook">\n' ; jupyter nbconvert $1 --to markdown --stdout; echo '</span>\n' ; } | pbcopy
+#!/usr/bin/env bash
+{
+    now=$(date +"%Y-%m-%d") ;
+    echo -e "<!-- Generated from $1 on $now -->" ;
+    echo -e '<span class="markdown--jupyter-notebook">\n' ;
+    jupyter nbconvert $1 --to markdown --stdout;
+    echo -e '</span>\n' ;
+} | pbcopy
 echo 'Your clipboard is filled with delicious notebook markdown'
