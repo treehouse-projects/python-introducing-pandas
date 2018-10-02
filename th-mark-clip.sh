7eab9039d4c1a74281e7c0f10e6fbf457c16ebf2
craigsdennis <craig@teamtreehouse.com>
2018-10-01 17:10:22 -0700

    Adds wrapper we need to format properly in Instruction steps

--- th-mark-clip.sh
+++ th-mark-clip.sh
@@ -1,1 +1,2 @@
-jupyter nbconvert $1 --to markdown --stdout|pbcopy
+{ echo '<span class="markdown--jupyter-notebook">\n' ; jupyter nbconvert $1 --to markdown --stdout; echo '</span>\n' ; } | pbcopy
+echo 'Your clipboard is filled with delicious notebook markdown'
