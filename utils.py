6c25d3fb481c8c217899b2e95e777d4849a63a24
craigsdennis <craig@teamtreehouse.com>
2018-10-03 13:34:40 -0700

    Introduce chaos and text manipulation

--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ from IPython.display import display, Markdown
 
 def render(md):
     return display(Markdown(md))
+
+def make_chaos(df, sample_size, columns, fn):
+    # Keep chaos the same randomly
+    some = df.sample(sample_size, random_state=sample_size)
+    for col in columns:
+        some[col] = some[col].apply(fn)
+    # Update the original DataFrame
+    df.update(some)
