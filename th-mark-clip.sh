5fe80366c54019a8200602bce509fa85a2dd153f
craigsdennis <craig@teamtreehouse.com>
2018-10-06 21:35:56 -0700

    Add auto-binder badge to markdown

--- th-mark-clip.sh
+++ th-mark-clip.sh
@@ -2,6 +2,7 @@
 {
     now=$(date +"%Y-%m-%d") ;
     echo -e "<!-- Generated from $1 on $now -->" ;
+    echo -e "[![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/treehouse-projects/python-introducing-pandas/master?filepath=$1)\n"
     jupyter nbconvert $1 --to markdown --stdout;
     echo -e "\n\n\n" ;
     cat ./th-script-snippet.html ;
